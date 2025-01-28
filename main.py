50d6f49a3386f6f9d98b6826b0a0fecf8a6bf29b
Shaik Mohammed Kuber <shaik.mohammed@visionetsystems.com>
2025-01-28 20:48:18 +0530

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+# main.py
+from azure.functions import AsgiMiddleware
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+@app.get("/hello")
+async def root2():
+    return {"message1": "Hello, World!2"}
+
+
+@app.get("/welcome")
+async def root2():
+    return {"message3": "welcome"}
+
+main = AsgiMiddleware(app)
+
